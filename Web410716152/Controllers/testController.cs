d6d34e21b1812ebdb17d4d332a9cb81f6774af6a
羅永禎 <harry20000609ss@gmail.com>
2021-05-06 20:28:41 +0800

    HW

---
+++ Web410716152/Controllers/testController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410716152.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: test
+        public ActionResult HW1()
+        {
+            return View();
+        }
+    }
+}
